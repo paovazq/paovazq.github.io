e4746a210bdd32a9b765edf26aa365511e77ce9a
dannyhp1 <dpham760@gmail.com>
2020-01-03 20:25:51 -0800

    Added youtube player.

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+/* For scrolling through YT thumbnails */
+$(document).ready(function () {
+  $('.vid-item').each(function(index){
+    $(this).on('click', function(){
+      var current_index = index+1;
+      $('.vid-item .thumb').removeClass('active');
+      $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
+    });
+  });
+});
+
 /* Animation to scroll from Introduction to Contact Me */
 $('#contactme').click(function() {
   $('html,body').animate({
